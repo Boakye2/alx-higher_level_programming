e6c53c268b2e5fc53ae8ec1aeb65656384cd9d63
Boakye2 <nanaboakyefred2@gmail.com>
2022-07-07 02:56:02 +0200

    0x0B

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Boakye
+""" File name : 12-student.py
+"""
+
+
+class Student(object):
+    """Class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """__init__ initialized constructor
+
+        Args:
+            first_name (str): first name
+            last_name (str: last name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary represt """
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dictionary[key] = value
+        return dictionary
