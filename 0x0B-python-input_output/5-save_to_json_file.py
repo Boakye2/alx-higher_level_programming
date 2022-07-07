2a8dcc706192884d190844f38023ef255dab04eb
Boakye2 <nanaboakyefred2@gmail.com>
2022-07-07 02:36:06 +0200

    0x0B

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Boakye
+""" File name : 5-to_json_string.py
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """to_json_string  returns the JSON representation of an object (string)
+
+    Args:
+        my_obj (obj): any object for example list, dict
+    """
+    return(json.dumps(my_obj))
