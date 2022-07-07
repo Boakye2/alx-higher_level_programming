bbb072a1428a02b6a6cff74c791e0b7aaed5f9fe
Boakye2 <nanaboakyefred2@gmail.com>
2022-07-07 02:29:20 +0200

    0x0B

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Boakye
+""" File name : 0-read_file.py
+    Use the with statement
+    It is not allowed to import any module
+"""
+
+
+def read_file(filename=""):
+    """read_file reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str): content of the file. Defaults to "".
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
