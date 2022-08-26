f98ba580943330a79b244f67b468ee1523d7421e
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-08-26 03:43:21 +0200

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Script that create the MySQL server user user_0d_1
-
+-- Create the MySQL server user user_0d_1
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
