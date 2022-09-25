fa2a4b6aac4408f0386fa575eabe1ae0923d77d5
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-09-25 16:30:08 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://alx-intranet.hbtn.io/status
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
-from urllib.request import Request, urlopen
-
-
 if __name__ == "__main__":
-    req = Request('https://alx-intranet.hbtn.io/status')
+    from urllib import request
 
-    with urlopen(req) as res:
-        content = res.read()
-        utf8_content = content.decode('utf-8')
-
+    thehtml = ""
+    theurl = "https://alx-intranet.hbtn.io/status"
+    req = request.Request(theurl)
+    with request.urlopen(req) as response:
+        thehtml = response.read()
         print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+        print("\t- type: {}".format(type(thehtml)))
+        print("\t- content: {}".format(thehtml))
+        print("\t- utf8 content: {}".format(thehtml.decode('utf-8')))
