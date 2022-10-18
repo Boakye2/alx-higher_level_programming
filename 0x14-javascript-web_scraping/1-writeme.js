2759ec1491d064c2d3b76054ec178d9de078d35a
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-10-18 21:29:33 +0200

    script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/nodejs
+/**
+ *
+ * Script that writes a string to a file.
+ */
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
