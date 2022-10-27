b8c18fc9c86d9a8641a7c573e5e701674b30bf73
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-10-27 02:29:27 +0200

    Create 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // To runs when the DOM is ready, i.e. when the document has been parsed
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
