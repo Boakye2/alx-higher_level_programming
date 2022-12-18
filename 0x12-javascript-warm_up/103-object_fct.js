c8d8c76d563b7f42c6426002dfb41f7dc7bb2953
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-12-18 12:46:51 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
-
+/* Update this script by adding a new function
+incr that increments the integer value. */
 const myObject = {
   type: 'object',
   value: 12
 };
-
 console.log(myObject);
-
 myObject.incr = function () {
-  ++this.value;
+  this.value++;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
