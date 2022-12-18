6776f4f9c72598396d3e224b9823be2e915399b3
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-12-18 13:12:09 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
-#!/usr/bin/nodejs
-/* Script that prints 3 lines using an array of strings */
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const idx in array) {
-  console.log(array[idx]);
+#!/usr/bin/node
+/* script that prints 3 lines: (like 1-multi_languages.js)
+but by using an array of string and a loop */
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
 }
