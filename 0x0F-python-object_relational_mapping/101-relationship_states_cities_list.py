1f6cc6305bca5b3e997dfb748755d322079ac843
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-12-19 16:11:11 +0100

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""
-    script that lists all State objects, and corresponding City objects,
-    contained in the database hbtn_0e_101_usa
-"""
+# Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
+# Usage: ./101-relationship_states_cities_list.py <mysql username> /
+#                                                 <mysql password> /
+#                                                 <database name>
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    start = sessionmaker()
-    start.configure(bind=engine)
-    session = start()
-    stmt = session.query(State).order_by(State.id)
-    for i in stmt:
-        print("{:d}: {:s}".format(i.id, i.name))
-        for j in i.cities:
-            print("\t{:d}: {:s}".format(j.id, j.name))
-    session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+            
